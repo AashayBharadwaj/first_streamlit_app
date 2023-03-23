e939cfcf9e044507e8171a50a4debb1ba9e0aa4e
Aashay Bharadwaj <aashay.bharadwaj@gmail.com>
2023-03-22 22:54:26 -0500

    Update streamlit_app.py
    Added some comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,20 @@
 
 import streamlit
+
+# Entering text on the page
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-   
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spniach & Rocket Smoothie')
 streamlit.text('🐔Hard Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avacado Toast')
 
+#  Importing pandas and reading a CSV file
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # streamlit.dataframe(my_fruit_list)
+
+# Set an index to use it for displaying selections
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
